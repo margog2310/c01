d0fb90c7c6837c00ed037b635dbe31f7ece7477c
margog2310 <margaritaigancheva@gmail.com>
2025-05-01 23:19:03 +0100

    fixed some minor errors

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,9 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mganchev <mganchev@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/01 20:09:08 by mganchev          #+#    #+#             */
+/*   Updated: 2025/05/01 20:16:27 by mganchev         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "Zombie.hpp"
 
 int main()
 {
     Zombie* heapZombie = newZombie("heapZombie");
     randomChump("stackZombie");
+    heapZombie->announce();
     delete heapZombie;
     return (EXIT_SUCCESS);
 }
